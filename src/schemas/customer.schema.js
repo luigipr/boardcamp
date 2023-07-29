e0bd9ce887bca636333c8fd24d46008851b2b658
luigipr <luigiprofeti@gmail.com>
2023-07-29 00:28:21 -0300

    fix/ schemas

--- src/schemas/customer.schema.js
+++ src/schemas/customer.schema.js
@@ -6,7 +6,7 @@ const joi = Joi.extend(joiDate)
 export const customerSchema = joi.object({
     name: joi.string().required(),
     phone: joi.string().required(),
-    cpf: joi.string().length(11).required(),
+    cpf: joi.string().length(11).pattern(/^[0-9]+$/).required(),
     birthday: joi.date().format('YYYY-MM-DD').required()
     //birthday: joi.string().isoDate().required()
    
